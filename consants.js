15c6518d5030d56dee36dc3db0d146abbbc15862
ahsan.btph123@gmail.com <ahsan.btph123@gmail.com>
2023-06-08 20:54:07 +0500

    first commit

---
+++ consants.js
@@ -0,0 +1,6 @@
+const ACCOUNT_MNEUMONIC =
+  "radar man physical twin demand tag hard milk solid south virtual pigeon";
+const GOERLI_URL =
+  "https://sepolia.infura.io/v3/b2c188a795e44ef7b9bf0b0982cf1eab";
+
+module.exports = { ACCOUNT_MNEUMONIC, GOERLI_URL };
